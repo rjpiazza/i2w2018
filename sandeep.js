74df91b6ae609fa2e3160f029fe85120b93aaedd
Sandeep K V <sandeepkv93@gmail.com>
2018-09-05 11:13:22 -0400

    Dynamic Loading of tensorflow js

--- sandeep.js
+++ sandeep.js
@@ -1,20 +1,51 @@
 console.log('sandeep.js is loaded');
 
-const model = tf.sequential();
+function loadScript(url, callback) {
 
-model.add(tf.layers.dense({
-    units: 1,
-    inputShape: [1]
-}));
+    var script = document.createElement("script")
+    script.type = "text/javascript";
 
-model.compile({
-    loss: 'meanSquaredError',
-    optimizer: 'sgd'
-});
-const xs = tf.tensor([1, 3, 5, 7], [4, 1]);
-const ys = tf.tensor([2, 4, 6, 8], [4, 1]);
+    if (script.readyState) {
+        //IE
+        script.onreadystatechange = function() {
+            if (script.readyState == "loaded" || script.readyState == "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        }
+        ;
+    } else {
+        //Others
+        script.onload = function() {
+            callback();
+        }
+        ;
+    }
 
-model.fit(xs, ys).then(()=>{
-    model.predict(tf.tensor([1, 2, 3, 4], [4, 1])).print();
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
-);
+
+function tensorflow_demo() {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+        units: 1,
+        inputShape: [1]
+    }));
+
+    model.compile({
+        loss: 'meanSquaredError',
+        optimizer: 'sgd'
+    });
+    const xs = tf.tensor([1, 3, 5, 7], [4, 1]);
+    const ys = tf.tensor([2, 4, 6, 8], [4, 1]);
+
+    model.fit(xs, ys).then(()=>{
+        model.predict(tf.tensor([1, 2, 3, 4], [4, 1])).print();
+    }
+    );
+}
+
+if (typeof tf === 'undefined') {
+    loadScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs', tensorflow_demo)
+}
