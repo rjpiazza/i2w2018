0f5523a9dd84945ff9ef849548b929d00e98408a
Sandeep K V <sandeepkv93@gmail.com>
2018-09-05 11:20:33 -0400

    Dynamic Loading of tensorflow js using script.onload

--- sandeep.js
+++ sandeep.js
@@ -4,22 +4,8 @@ function loadScript(url, callback) {
 
     var script = document.createElement("script")
     script.type = "text/javascript";
-
-    if (script.readyState) {
-        //IE
-        script.onreadystatechange = function() {
-            if (script.readyState == "loaded" || script.readyState == "complete") {
-                script.onreadystatechange = null;
-                callback();
-            }
-        }
-        ;
-    } else {
-        //Others
-        script.onload = function() {
-            callback();
-        }
-        ;
+    script.onload = function() {
+        callback();
     }
 
     script.src = url;
